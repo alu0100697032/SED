6e335c919fb53deba69f04b9800b79c7e753e82f
Víctor Hernández Pérez <alu0100697032@ull.edu.es>
2016-04-05 12:50:17 +0100

    Added files via upload

---
+++ practica3/decodificador2.vhd
@@ -0,0 +1,53 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    12:38:07 04/05/2016 
+-- Design Name: 
+-- Module Name:    decodificador2 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity decodificador2 is
+    Port ( bcd : in  STD_LOGIC_VECTOR (3 downto 0);
+           led : out  STD_LOGIC_VECTOR (6 downto 0));
+end decodificador2;
+
+architecture Behavioral of decodificador2 is
+
+begin
+	with bcd select 
+	led <= "1000000" when "0000", 
+			"1111001" when "0001",
+			"0100100" when "0010",
+			"0110000" when "0011",
+			"0011001" when "0100",
+			"0010010" when "0101",
+			"0000010" when "0110",
+			"1111000" when "0111",
+			"0000000" when "1000",
+			"0010000" when "1001",
+			"0111111" when others;
+end Behavioral;
+
